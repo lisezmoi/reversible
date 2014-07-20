f0105443128f3e032ec834668446c4787acab0b0
Raphaël Bastide <raphael.bastide@gmail.com>
2014-07-20 03:43:16 +0200

    Fancier boxes behaviour

--- src/ui.js
+++ src/ui.js
@@ -12,13 +12,27 @@ function toggleClass(elem, className) {
     elem.className += ' ' + className;
   }
 }
+
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+
 var aboutBox = document.querySelector('.about-box');
 var controlsBox = document.querySelector('.controls-box');
+var boxes = document.querySelector('box');
 
 document.getElementById('about').onclick = function() {
+  removeClass(controlsBox, 'opened');
   toggleClass(aboutBox, 'opened');
 }
 
 document.getElementById('controls').onclick = function() {
+  removeClass(aboutBox, 'opened');
   toggleClass(controlsBox, 'opened');
 }
