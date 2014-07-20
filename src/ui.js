b94b27202372dfd8138d0ff9e8c9109fcca8d283
Pierre Bertet <bonjour@pierrebertet.net>
2014-07-21 00:52:31 +0100

    Start screen

--- src/ui.js
+++ src/ui.js
@@ -4,6 +4,15 @@ module.exports = function init(cb) {
   var controlsBox = document.querySelector('.controls-box');
   var aboutBtn = document.querySelector('#about');
   var controlsBtn = document.querySelector('#controls');
+  var startBox = document.querySelector('.start-box');
+
+  startBox.classList.add('opened');
+  startBox.onclick = function() {
+    controlsBox.classList.remove('opened');
+    aboutBox.classList.remove('opened');
+    startBox.classList.remove('opened');
+    cb(false);
+  };
 
   aboutBox.onclick = controlsBox.onclick = function() {
     about.classList.remove('opened');
@@ -14,12 +23,12 @@ module.exports = function init(cb) {
   aboutBtn.onclick = function() {
     var opened = aboutBox.classList.toggle('opened');
     controlsBox.classList.remove('opened');
-    cb(opened);
+    cb(opened || startBox.classList.contains('opened'));
   };
 
   controlsBtn.onclick = function() {
     var opened = controlsBox.classList.toggle('opened');
     aboutBox.classList.remove('opened');
-    cb(opened);
+    cb(opened || startBox.classList.contains('opened'));
   };
 };
