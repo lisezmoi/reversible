4ec71195ad2fb057e505aabdb3e4c00706d681d9
Raphaël Bastide <raphael.bastide@gmail.com>
2014-07-21 02:56:09 +0200

    Sound, player, minor css

--- bundle.js
+++ bundle.js
@@ -1742,24 +1742,6 @@ module.exports = {
 };
 
 },{}],11:[function(require,module,exports){
-onclick=".pause()";
-
-var audioCtrl = document.querySelector('.audioCtrl');
-var playBtn = document.querySelector('.play');
-var pauseBtn = document.querySelector('.pause');
-var player = document.getElementById('player');
-
-playBtn.onclick = function() {
-  player.play();
-  this.classList.add('hidden');
-  pauseBtn.classList.remove('hidden');
-};
-pauseBtn.onclick = function() {
-  player.pause();
-  this.classList.add('hidden');
-  playBtn.classList.remove('hidden');
-};
-
 module.exports = function init(cb) {
 
   var aboutBox = document.querySelector('.about-box');
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,21 @@
+onclick=".pause()";
+
+var audioCtrl = document.querySelector('.audioCtrl');
+var playBtn = document.querySelector('.play');
+var pauseBtn = document.querySelector('.pause');
+var player = document.getElementById('player');
+
+playBtn.onclick = function() {
+  player.play();
+  this.classList.add('hidden');
+  pauseBtn.classList.remove('hidden');
+};
+pauseBtn.onclick = function() {
+  player.pause();
+  this.classList.add('hidden');
+  playBtn.classList.remove('hidden');
+};
+
 module.exports = function init(cb) {
 
   var aboutBox = document.querySelector('.about-box');
