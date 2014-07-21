656ad6f5ac99966e500be8c76b53e7d2fab88727
Raphaël Bastide <raphael.bastide@gmail.com>
2014-07-21 04:24:39 +0200

    Sound icons, screen design

--- src/screens.js
+++ src/screens.js
@@ -4,13 +4,13 @@ module.exports = {
     var height = ctx.canvas.height;
     ctx.fillStyle = 'rgba(0,0,0,.65)';
     ctx.fillRect(0, 0, width, height);
-    ctx.font = '60px/100px Verdana, sans-serif';
+    ctx.font = '12px/19px Verdana, sans-serif';
     ctx.fillStyle = 'white';
     ctx.textAlign = 'center';
-    ctx.fillText('GAME PAUSED', width / 2, height / 2 - 20);
+    ctx.fillText('GAME PAUSED', width / 2, height / 2);
 
-    ctx.font = '20px/60px Verdana, sans-serif';
-    ctx.fillText('PRESS [P] TO RESUME', width / 2, height / 2 + 60);
+    ctx.font = '12px/19px Verdana, sans-serif';
+    ctx.fillText('PRESS [P] TO RESUME', width / 2, height / 2 + 120);
   },
   starting: function(ctx) {
     // TODO
@@ -20,17 +20,17 @@ module.exports = {
     var height = ctx.canvas.height;
     ctx.fillStyle = 'rgba(0,0,0,.65)';
     ctx.fillRect(0, 0, width, height);
-    ctx.font = '60px/100px Verdana, sans-serif';
+    ctx.font = '12px/19px Verdana, sans-serif';
     ctx.fillStyle = 'white';
     ctx.textAlign = 'center';
-    ctx.fillText('GAME OVER', width / 2, height / 2 - 20);
+    ctx.fillText('GAME OVER', width / 2, height / 2);
 
-    ctx.font = '20px/60px Verdana, sans-serif';
+    ctx.font = '12px/19px Verdana, sans-serif';
 
     var years = (Math.round(score * 1000) + '').replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-    ctx.fillText('EVOLUTION PERIOD: ' + years + ' YEARS', width / 2, height / 2 + 60);
+    ctx.fillText('EVOLUTION PERIOD: ' + years + ' YEARS', width / 2, height / 2 + 120);
 
-    ctx.font = '20px/60px Verdana, sans-serif';
-    ctx.fillText('PRESS [R] TO PLAY AGAIN', width / 2, height / 2 + 180);
+    ctx.font = '12px/19px Verdana, sans-serif';
+    ctx.fillText('PRESS [R] TO PLAY AGAIN', width / 2, height / 2 + 160);
   }
 };
